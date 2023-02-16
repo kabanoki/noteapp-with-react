7e0ead9329a56241bbc811b65c0fa8ed72456c05
kabanoki <info@kabanoki.net>
2023-02-16 11:55:31 +0900

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Main from './components/Main'
+import Sidebar from './components/Sidebar'
+import uuid from 'react-uuid'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [notes, setNotes] = useState([]);
+  const [activeNote, setActiveNote] = useState(false);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title:'新しいノート',
+      content: "ここに本文が入ります。",
+      modDate: Date.now()
+    };
+    console.log(notes);
+    setNotes([...notes, newNote])
+  }
+
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id)
+    setNotes(filterNotes);
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Sidebar onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote} activeNote={activeNote} setActiveNote={setActiveNote} />
+      <Main />
     </div>
   )
 }
